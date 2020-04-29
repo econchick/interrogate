d04f243a1bd38f0f46cb1c7c657f1c03b13a9c27
Lynn Root <lynn@lynnroot.com>
2020-04-29 10:18:04 -0400

    Actually bump version :facepalm:

--- src/interrogate/__init__.py
+++ src/interrogate/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Lynn Root
 """Explain yourself! Interrogate a codebase for docstring coverage."""
 __author__ = "Lynn Root"
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __email__ = "lynn@lynnroot.com"
 __description__ = "Interrogate a codebase for docstring coverage."
 __uri__ = "https://github.com/econchick/interrogate"
