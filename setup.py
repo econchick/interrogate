746dcff6b09c8c51890986047ef37273823b7c46
Lynn Root <lynn@lynnroot.com>
2022-07-29 11:45:20 -0400

    Update GH workflows

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: Implementation :: CPython",
     "Topic :: Software Development :: Documentation",
     "Topic :: Software Development :: Libraries :: Python Modules",
